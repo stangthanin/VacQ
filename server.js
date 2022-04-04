5f48da407dc6d1fab290d168bb56aa2cf018ef29
stangthanin <thanin.sawetkititham@gmail.com>
2022-04-05 02:25:01 +0700

    Add Swagger API docs

--- server.js
+++ server.js
@@ -8,8 +8,28 @@ const xss = require("xss-clean");
 const rateLimit = require("express-rate-limit");
 const hpp = require("hpp");
 const cors = require("cors");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 
 dotenv.config({ path: "./config/config.env" });
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express VacQ API",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/api/v1",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 connectDB();
 
@@ -19,7 +39,7 @@ const appointments = require("./routes/appointments");
 
 const limiter = rateLimit({
   windowsMs: 10 * 60 * 1000, //10 mins
-  max: 1,
+  max: 50,
 });
 
 const app = express();
@@ -32,6 +52,7 @@ app.use(xss());
 app.use(limiter);
 app.use(hpp());
 app.use(cors());
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.use("/api/v1/hospitals", hospitals);
 app.use("/api/v1/auth", auth);
